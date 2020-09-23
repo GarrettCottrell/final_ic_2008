4cfa065899a754a51d983b227ee1213fd8038d2b
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2020-09-23 09:20:36 -0600

    created test and method for stock_check

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -11,4 +11,18 @@ class PantryTest < Minitest::Test
 
     assert_instance_of Pantry, pantry
   end
+
+  def test_it_has_attributes
+    pantry = Pantry.new
+
+    assert_equal ({}), pantry.stock
+  end
+
+  def test_stock_check
+    pantry = Pantry.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+
+    assert_equal 0, pantry.stock_check(ingredient1)
+  end
 end
