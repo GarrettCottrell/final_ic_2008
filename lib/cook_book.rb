a07d2af4f5c627e176e9565196fc40e5ce597776
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2020-09-23 10:04:54 -0600

    created test for add recripe

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -1,10 +1,15 @@
 class CookBook
-  attr_reader :recipes
+  attr_reader :recipes, :ingredients
   def initialize
     @recipes = []
+    @ingredients = []
   end
 
   def add_recipe(recipe)
     @recipes << recipe
+  end
+
+  def add_recipe
+
   end
 end
